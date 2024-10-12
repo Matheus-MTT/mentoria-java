2222e2cd84cdd1ee9905d909b9a03029a87376b2
Matheus-MTT <matheusybn@gmail.com>
2024-10-12 09:40:07 -0300

    Apresentacao do problema

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,20 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    /**
+     * <h1>PROPOSTA DA ATIVIDADE</h1>
+     * <p>Em uma escola, foi estabelecido que o nosso professor vai poder colocar quantas vezes ele quiser uma
+     * nota de um aluno e vai ser necessário calcularmos a nota média que foi obtida
+     * e mostrar se o aluno ao longo do ano conseguiu bater a média que é de 6.</p>
+     *
+     * <h2>REQUISITOS QUE VÃO SER AVALIADOS</h>
+     * <ul>
+     *     <li>O sistema deve ter uma forma do professor digitar as notas dos alunos.</li>
+     *     <li>Todas essas notas vão ser armazenadas em uma parte no código.</li>
+     *     <li>Calcular a média de todas essas notas.</li>
+     *     <li>Mostra se ele foi aprovado ou não.</li>
+     * </ul>
+     */
     public static void main(String[] args) {
 
     }
